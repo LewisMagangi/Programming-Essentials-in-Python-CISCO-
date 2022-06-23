94c5f718992c134d3ee3717d2bd7be3ae839f3fc
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-23 09:05:16 -0700

    lab work complete

---
+++ Module2/Basic-IO-Operations/10-basic-io-lab.py
@@ -0,0 +1,9 @@
+a = 50.5# input a float value for variable a here
+b = 5.50# input a float value for variable b here
+
+print(a + b)# output the result of addition here
+print(a - b)# output the result of subtraction here
+print(a * b)# output the result of multiplication here
+print(9 / 7)# output the result of division here
+
+print("\nThat's all, folks!")
