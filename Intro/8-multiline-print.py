828e38b187f2703580203f29baa46cb63fc6a2a3
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-24 01:48:34 -0700

    printing multiple lines

---
+++ Intro/8-multiline-print.py
@@ -0,0 +1,10 @@
+print(
+    """
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
