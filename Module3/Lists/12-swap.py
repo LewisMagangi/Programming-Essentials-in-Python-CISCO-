95d5100f7d63c45045337163cd63b80a365df9d4
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-25 01:56:53 -0700

    swaping in python

---
+++ Module3/Lists/12-swap.py
@@ -0,0 +1,11 @@
+variable_1 = 123
+variable_2 = 243
+print("Var 1 :", variable_1)
+print("Var 2 :", variable_2)
+
+temp = variable_1
+variable_1 = variable_2
+variable_2 = temp
+
+print("Var 1 :", variable_1)
+print("Var 2 :", variable_2)
---
+++ Module3/Lists/13-swap2.py
@@ -0,0 +1,10 @@
+variable_1 = 101
+variable_2 = 202
+
+print("Var 1 :", variable_1)
+print("Var 2 :", variable_2)
+
+variable_1, variable_2 = variable_2, variable_1
+
+print("Var 1 :", variable_1)
+print("Var 2 :", variable_2)
