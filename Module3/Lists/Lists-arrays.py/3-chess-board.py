6f4b912a1439baf85fefe22c7cf71683683f851b
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-25 10:18:51 -0700

    temp programs

---
+++ Module3/Lists/Lists-arrays.py/3-chess-board.py
@@ -0,0 +1,29 @@
+EMPTY = "-"
+ROOK = "ROOK"
+board = []
+break_loop = True
+
+for i in range(8):
+    row = [EMPTY for i in range(8)]
+    board.append(row)
+
+board[0][0] = ROOK
+board[0][7] = ROOK
+board[7][0] = ROOK
+board[7][7] = ROOK
+
+for i in range(8):
+    while break_loop:
+        for j in range(8):
+            while break_loop:
+                if board[i][j] == "-":
+                    answer = input("Do you want to fill the board with a piece ?, Type : Yes or No :")
+                    if answer == "No":
+                        print("Great nice time !")
+                        break_loop = False
+                    elif answer == "Yes":
+                        board[i][j] = input("Kindly enter a valid chess piece to be inputed :")
+                        print("The ", board[i][j], "has been entered successfully !")
+                    else:
+                        print("That/'s not a valid choice, Kindly enter a choice again")
+print(board)
