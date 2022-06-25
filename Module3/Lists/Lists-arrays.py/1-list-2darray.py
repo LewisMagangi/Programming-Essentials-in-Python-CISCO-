cb0fc4c2d05332dbb14848252f47b170f3840351
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-25 08:31:41 -0700

    two dimension arrays

---
+++ Module3/Lists/Lists-arrays.py/1-list-2darray.py
@@ -0,0 +1,7 @@
+board = []
+
+EMPTY = "block"
+for i in range(8):
+    row = [EMPTY for i in range(8)]
+    board.append(row)
+print(board)
---
+++ Module3/Lists/Lists-arrays.py/2-list-2darray.py
@@ -0,0 +1,5 @@
+board = []
+EMPTY = "block"
+
+board = [[EMPTY for i in range(8)] for j in range(8)]
+print(board)
