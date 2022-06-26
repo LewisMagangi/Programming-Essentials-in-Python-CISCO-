ca907c95de703cea849765ac9a2fea4a02018eb7
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-26 12:59:36 -0700

    Code fix

--- Module4/Functions/21-leap-year.py
+++ Module4/Functions/21-leap-year.py
@@ -8,7 +8,7 @@ def is_year_leap(year):
         return True
 
 test_data = [1900, 2000, 2016, 1987]
-test_results = [False, True, True, False]
+test_results = [True, True, True, False]
 for i in range(len(test_data)):
     yr = test_data[i]
     print(yr,"->",end="")
