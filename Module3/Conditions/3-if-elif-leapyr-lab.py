b1871eaa194d7c368939dab421a4fc3557013450
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-27 02:27:34 -0700

    The previous test cases didn't cover  all the assumptions 1900 test value had a wroong output, the logical error is now fixed

--- Module3/Conditions/3-if-elif-leapyr-lab.py
+++ Module3/Conditions/3-if-elif-leapyr-lab.py
@@ -2,9 +2,10 @@ year = int(input("Enter a year: "))
 
 if year < 1582:
     print("Not within the Gregorian calendar period")
-elif (year % 400) != 0 and (year % 4) != 0:
-    print("Common year")
-elif (year % 100) != 0:
-    print("Leap year")
+elif (year % 4) == 0:
+    if year % 100 == 0 and year % 400 != 0:
+        print("Common year")
+    else:
+        print("Leap year")
 else:
-    print("Leap year")
+    print("Common year")
