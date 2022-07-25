c86cf5590459c50830119580a0c6ce19614642cf
Liquelaliqour <lewismomanyi34@gmail.com>
2022-07-25 07:37:09 +0000

    my wishes to you

---
+++ Module4/Functions/26-var-result.py
@@ -0,0 +1,14 @@
+# Example 1
+def wishes():
+    print("My Wishes")
+    return "Happy Birthday"
+
+wishes()   
+
+# Example 2
+def wishes():
+    print("My Wishes")
+    return "Happy Birthday"
+
+print(wishes())
+
