9d648dc3bc688eb54630ba4cd3f24baf711f844a
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-02 10:50:03 +0000

    using dictionaries

---
+++ Module4/Dictionaries/1-using-dict.py
@@ -0,0 +1,6 @@
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+phone_numbers = {'boss' : 5551234567, 'Suzy' : 22657854310}
+empty_dictionary = {}
+
+print(dictionary['cat'])
+print(phone_numbers['Suzy'])    # Print the values here.
