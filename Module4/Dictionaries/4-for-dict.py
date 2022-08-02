ee417792be31a7e831cae0179fd201fa10b874ae
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-02 13:06:50 +0000

    pushing dictionaries

--- Module4/Dictionaries/4-for-dict.py
+++
@@ -1,4 +0,0 @@
-dictionary = {"cat": "kanyau", "dog": "mbwa", "horse": "farasi"}
-
-for key in dictionary.keys():
-    print(key, "->", dictionary[key])
---
+++ Module4/Dictionaries/5-sort-func-dict.py
@@ -0,0 +1,1 @@
+for key in sorted(dictionary.keys()):
