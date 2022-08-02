9d10b4357dd52b798fa9f7666da0e803bc6829d2
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-02 13:13:47 +0000

    using sorted() function to sort a dictionary

--- Module4/Dictionaries/5-sort-func-dict.py
+++ Module4/Dictionaries/5-sort-func-dict.py
@@ -1,1 +1,9 @@
+dictionary = {"cat": "kanyau", "dog": "mbwa", "horse": "farasi"}
+
+for key in dictionary.keys():
+    print(key, "->", dictionary[key])
+
+print("Sorted Dictionary :")
+
 for key in sorted(dictionary.keys()):
+    print(key, "->", dictionary[key])
