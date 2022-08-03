f2150d5a5ffd333f521594a6822fbf8a7b004d9d
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-03 05:46:57 +0000

    a swahili kamusi

---
+++ Module4/Dictionaries/17-Update-Popitem.py
@@ -0,0 +1,7 @@
+pol_swahili_dictionary = {"maua": "flower"}
+
+pol_swahili_dictionary.update({"mchanga": "soil"})
+print(pol_swahili_dictionary)    # outputs: {'kwiat': 'flower', 'gleba': 'soil'}
+
+pol_swahili_dictionary.popitem()
+print(pol_swahili_dictionary)    
