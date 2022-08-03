37e96d0d432d073a589136d09de4e01620afb1c8
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-03 05:52:42 +0000

    for loop in elements

---
+++ Module4/Dictionaries/18-for-elem.py
@@ -0,0 +1,11 @@
+pol_swahili_dictionary = {
+    "jumba": "castle",
+    "maji": "water",
+    "mchanga": "soil"
+    }
+
+pol_swahili_dictionary["maji"] = "waba"
+print(pol_swahili_dictionary["maji"])    
+
+for item in pol_swahili_dictionary:
+    print(item) 
