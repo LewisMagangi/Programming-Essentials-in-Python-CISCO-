78e498fd85a2def9e4b9718dd7d26cde08773c3c
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-03 05:33:14 +0000

    swahili dictionary

---
+++ Module4/Dictionaries/15-Modifying-Keys.py
@@ -0,0 +1,8 @@
+pol_swahili_dictionary = {
+    "jumba": "castle",
+    "maji": "water",
+    "mchanga": "soil"
+    }
+
+pol_swahili_dictionary["maji"] = "waba"
+print(pol_swahili_dictionary["maji"])    
