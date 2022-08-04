4c09b7da9bdd4082d9f3e29d37a9b9e789d7ff0c
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-04 14:02:25 +0000

    fixing some logical bugs

--- Module4/Dictionaries/21-Del-Clear.py
+++ Module4/Dictionaries/21-Del-Clear.py
@@ -1,6 +1,3 @@
-global pol_swahili_dictionary
-global i
-i = 1
 pol_swahili_dictionary = {
     "jumba": "castle",
     "maji": "water",
@@ -8,21 +5,25 @@ pol_swahili_dictionary = {
     }
 
 def Printkeys(pol_swahili_dictionary):
+    i = 1
     for item in pol_swahili_dictionary:
         print(i, ": ", item)
         i += 1
 
 del pol_swahili_dictionary["maji"]    # remove an item
 print("After using the del keyword :")    # outputs: 2
-Printkeys(pol_swahili_dictionary ) 
+Printkeys(pol_swahili_dictionary) 
     
 pol_swahili_dictionary.clear()   # removes all the items
-print("After using the clear() function")    # outputs: 0
-Printkeys(pol_swahili_dictionary )
+print("After using the clear() method")    # outputs: 0
+Printkeys(pol_swahili_dictionary)
 
 print("The length of the dictionary is :", len(pol_swahili_dictionary))
 print(pol_swahili_dictionary)
 Printkeys(pol_swahili_dictionary)
  
 del pol_swahili_dictionary   # removes the dictionary
-Printkeys(pol_swahili_dictionary) 
+try:
+    Printkeys(pol_swahili_dictionary) 
+except:
+    print("pol_swahili_dictionary Not found") 
