b84931a09da3bf5c9db8e7d6e37a79e00f95d2b6
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-08 20:25:41 +0000

    working on victory function

--- Module4/Tic-Tac-Toe.py
+++ Module4/Tic-Tac-Toe.py
@@ -39,21 +39,48 @@ def display_board(board):
     """The function accepts one parameter containing the board's current status
        and prints it out to the console."""
 
-
 def enter_move(board):
-    # The function accepts the board's current status, asks the user about their move, 
-    # checks the input, and updates the board according to the user's decision.
-
+    value = input("Enter your move: ")
+    for i in range(3):
+        for j in range(3):
+            if value == board[i][j]:
+                board[i][j] = "O"
+        
+    """The function accepts the board's current status, asks the user about their move, 
+       checks the input, and updates the board according to the user's decision."""
 
 def make_list_of_free_fields(board):
-    # The function browses the board and builds a list of all the free squares; 
-    # the list consists of tuples, while each tuple is a pair of row and column numbers.
-
+    for i in range(3):
+        for j in range(3):
+            if type(board[i][j]) is int:
+                free_field = []
+                free_field.append(board[i][j])
+                    
+    """The function browses the board and builds a list of all the free squares; 
+       the list consists of tuples, while each tuple is a pair of row and column numbers."""
 
 def victory_for(board, sign):
+    if board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O":
+        print("You won!")
+    elif board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
+        print("You won!")
+    elif board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O":
+         print("You won!")
+    elif board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O":
+         print("You won!")
+    elif board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O":
+         print("You won!")
+    elif board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O":
+         print("You won!")
+    elif board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O":
+         print("You won!")
+    elif board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O":
+         print("You won!")
+    
+    
     # The function analyzes the board's status in order to check if 
     # the player using 'O's or 'X's has won the game
 
-
 def draw_move(board):
     # The function draws the computer's move and updates the board.
+
