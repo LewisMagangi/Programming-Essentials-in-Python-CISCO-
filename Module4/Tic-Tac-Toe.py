f8d5ac4e55b4119055420a8aaa8b2fca5cd777b5
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-08 19:21:27 +0000

    wow

--- Module4/Tic-Tac-Toe.py
+++ Module4/Tic-Tac-Toe.py
@@ -1,6 +1,43 @@
+board = [
+    [1, 2, 3],
+    [4, "X", 6],
+    [7, 8, 9],
+] 
+
 def display_board(board):
-    # The function accepts one parameter containing the board's current status
-    # and prints it out to the console.
+    def first_line (board):
+        print("+-------+-------+-------+")
+    def second_line (board):
+        print("|       |       |       |")
+    def blank_line (board):
+        print("                         ")
+    first_line(board)
+    blank_line(board)
+    second_line(board)
+    blank_line(board)
+    print("|   " + str(board[0][0]) + "   |   " + str(board[0][1]) + "   |   " + str(board[0][2]) + "   |")
+    blank_line(board)
+    second_line(board)
+    blank_line(board)
+    first_line(board)
+    blank_line(board)
+    second_line(board)
+    blank_line(board)
+    print("|   " + str(board[1][0]) + "   |   " + str(board[1][1]) + "   |   " + str(board[1][2]) + "   |")
+    blank_line(board)
+    second_line(board)
+    blank_line(board)
+    first_line(board)
+    blank_line(board)
+    second_line(board)
+    blank_line(board)
+    print("|   " + str(board[2][0]) + "   |   " + str(board[2][1]) + "   |   " + str(board[2][2]) + "   |")
+    blank_line(board)
+    second_line(board)
+    blank_line(board)
+    first_line(board)
+    """The function accepts one parameter containing the board's current status
+       and prints it out to the console."""
 
 
 def enter_move(board):
