dbecc94f8b2d76da8d0f5fa85607b24d2bf6dadb
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-09 07:44:21 +0000

    securing all exceptions

--- Module4/enter_move.py
+++ Module4/enter_move.py
@@ -5,10 +5,20 @@ board = [
 ]
 
 def enter_move(board):
-    value = int(input("Enter your move: "))
-    for i in range(3):
-        for j in range(3):
-            if value == board[i][j]:
-                board[i][j] = "O"
+    try:
+        value = int(input("Enter your move: "))
+    except ValueError:
+        print("Please enter an interger !")
+    try:
+        if value <= 0 or value >= 10:
+            print("Please enter a value in the range 1 to 9 !")
+    except UnboundLocalError:
+        print("The value entered is not applicable !")
+    else:
+        for i in range(3):
+            for j in range(3):
+                if value == board[i][j]:
+                    board[i][j] = "O"
+    return board
 enter_move(board)
 print(board)
