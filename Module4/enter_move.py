c09da771862271a3e684844e40e5c5b362d976b1
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-09 08:01:57 +0000

    free fields

--- Module4/enter_move.py
+++ Module4/enter_move.py
@@ -4,7 +4,16 @@ board = [
     [7, 8, 9],
 ]
 
-def enter_move(board):
+
+def make_list_of_free_fields(board):
+    free_field = []
+    for i in range(3):
+        for j in range(3):
+            if type(board[i][j]) is int:
+                free_field.append(board[i][j])
+    return free_field
+
+def enter_move(board, free_field):
     try:
         value = int(input("Enter your move: "))
     except ValueError:
@@ -14,11 +23,15 @@ def enter_move(board):
             print("Please enter a value in the range 1 to 9 !")
     except UnboundLocalError:
         print("The value entered is not applicable !")
-    else:
+    make_list_of_free_fields(board)
+    if value in free_field:
         for i in range(3):
             for j in range(3):
                 if value == board[i][j]:
                     board[i][j] = "O"
+    else:
+        print("The square is occupied, Kindly choose another square !")
     return board
-enter_move(board)
-print(board)
+make_list_of_free_fields(board)
+enter_move(board, free_field)
+print(board, free_field)
